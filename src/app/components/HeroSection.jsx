ff6dcb79c4e2ee1ca19726f1c96b668872e19461
Revathy <revathymr916@gmail.com>
2024-07-22 02:30:39 +0530

    Added 3d robot

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import { AnimatedModalDemo } from "./AnimatedModel";
+import Spline from "@splinetool/react-spline";
 
 const HeroSection = () => {
   return (
@@ -52,14 +53,15 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
-            <Image
+          <div className=" w-[250px] h-[250px] lg:w-[400px] lg:h-[500px] relative">
+            {/* <Image
               src="/img/home-img.png"
               alt="Robot holding peace sign"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
-            />
+            /> */}
+            <Spline className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/3 left-1/2" scene="https://prod.spline.design/qY5v4aeB1NZCTv5G/scene.splinecode"/>
           </div>
         </motion.div>
       </div>
